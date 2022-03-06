d1db50ac9c9364e711554eba8ea4f8551d6f9f63
Ali <Alihosseinzadeh1379ahz@gmail.com>
2022-03-06 16:18:58 +0330

    Course and Comment models created - Report model updated.

--- report_automation/models.py
+++ report_automation/models.py
@@ -1,15 +1,48 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime, timedelta
 
-# Create your models here.
+
 class Report(models.Model):
     number = models.PositiveSmallIntegerField()
     date = models.DateField()
-    student = models.ForeignKey(User, on_delete=models.CASCADE)
+    student = models.ForeignKey(User, on_delete=models.CASCADE, related_name='student_reports')
     description = models.TextField(default='empty')
-    study_time = models.TimeField()
+    study_duration = models.DurationField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-updated_at',)
+
+    def __str__(self):
+        return f'report num. {self.number} - {self.student}'
+
+
+
+class Comment(models.Model):
+    student = models.ForeignKey(User,  limit_choices_to={'groups__name': "students"}, on_delete=models.CASCADE, related_name='student_comments')
+    teacher = models.ForeignKey(User,  limit_choices_to={'groups__name': "teachers"}, on_delete=models.CASCADE, related_name='teacher_comments')
+    month_number = models.PositiveSmallIntegerField()
+    text = models.TextField(default='empty')
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.teacher.username} commented this for {self.student.username} in {self.created_at}'
+
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    number = models.PositiveSmallIntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description = models.TextField(null=True)
+    students = models.ManyToManyField(User)
+    teacher = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='teacher_courses')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'report num. {self.number} - {self.student}'
+        return f'{self.title} - {self.number}'
